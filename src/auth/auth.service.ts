844b95f3063eb277e1ed083adc3c33d6707cea61
ju-ki <juki.programming@gmail.com>
2024-03-06 22:47:37 +0900

    editorconfig追加

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,27 +9,43 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     try {
-
       const hash = await argon.hash(dto.password);
-  
+
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash: hash,
         },
       });
-  
+
       delete user.hash;
       return user;
     } catch (err) {
-      if(err instanceof PrismaClientKnownRequestError) {
-        if(err.code === 'P202002') {
-          throw new ForbiddenException('Crediential Error');
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P202002') {
+          throw new ForbiddenException('credential Taken');
         }
+      }
+      throw err;
     }
   }
-  signin(dto: AuthDto) {
-    return dto;
+  async signin(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      console.log(user);
+
+      if (!user) {
+        throw new ForbiddenException('Credentials incorrect');
+      }
+      return dto;
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
